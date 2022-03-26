48f8637e9f14e001a4b18743ac0671dcaeb94821
Tsuyoshi Yamamoto <tsuyoshi.yamamoto@jp.ricoh.com>
2022-03-26 14:41:36 +0900

    モジュール定義で追加したstruct、enumのサンプルを写経

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -18,3 +18,46 @@ pub fn eat_at_restaurant() {
     // 相対パス
     front_of_house::hosting::add_to_waitlist();
 }
+
+fn serve_order() {}
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+    fn cook_order() {}
+}
+
+pub fn order_breakfast() {
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+
+    println!("I'd like {} toast please.", meal.toast);
+
+    // seasonal_fruitは非公開なのでコンパイルエラー
+    // meal.seasonal_fruit = String::from("blueberries");
+}
+
+pub fn order_appetizer() {
+    let order1 = back_of_house::Appetizer::Salad;
+    let order2 = back_of_house::Appetizer::Soup;
+}
